1562bf4aefaeadaaec803ed102628ba21c345d21
Ryan DeMayo <164783214+Ryeandmayo@users.noreply.github.com>
2024-04-13 21:47:57 -0400

    Add files via upload

---
+++ Snake_Game/Snake_Game/snake.py
@@ -0,0 +1,54 @@
+from turtle import Turtle
+import random
+STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+colors= ["yellow", "gold", "orange", "red", "maroon", "violet", "magenta", "purple", "navy", "blue", "skyblue", "cyan", "turquoise", "lightgreen", "green", "darkgreen", "chocolate", "brown", "gray", "white"]
+class Snake:
+
+    def __init__(self):
+            self.segments = []
+            self.create_snake()
+            self.head = self.segments[0]
+    def create_snake(self):
+        for position in STARTING_POSITION:
+            self.add_segment(position)
+
+    def extend(self):
+        #add a new segment to snake
+        self.add_segment(self.segments[-1].position())
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color(random.choice(colors))
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def move(self):
+            for seg_num in range(len(self.segments) - 1, 0, -1):
+                new_x = self.segments[seg_num - 1].xcor()
+                new_y = self.segments[seg_num - 1].ycor()
+                self.segments[seg_num].goto(new_x, new_y)
+            self.segments[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.segments[0].seth(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.segments[0].seth(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.segments[0].seth(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.segments[0].seth(RIGHT)
+
+
